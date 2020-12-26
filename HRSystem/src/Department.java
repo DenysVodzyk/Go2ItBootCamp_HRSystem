b13a3c49ebabac0693743f67bdcfd490e86bfa4c
DenysVodzyk <vodzyk@gmail.com>
2020-12-25 19:47:58 -0500

    Department class modified

--- HRSystem/src/Department.java
+++ HRSystem/src/Department.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Department {
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Department {
+    private Set<Employee> employees = new HashSet<>();
+    private String departmentName;
+
+
+    public Department() {
+    }
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public void addEmployee(Employee employee) throws EntryAlreadyExistException {
+        if (employees.contains(employee)) {
+            throw new EntryAlreadyExistException("Employee is already part of the " + this.departmentName + " department.");
+        }
+        employees.add(employee);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return employees.equals(that.employees) &&
+                departmentName.equals(that.departmentName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employees, departmentName);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "employees=" + employees +
+                ", departmentName='" + departmentName + '\'' +
+                '}';
+    }
 }
