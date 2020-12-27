de18b5c32002207077b941b483bf483ebc3f2e4d
DenysVodzyk <vodzyk@gmail.com>
2020-12-27 12:43:41 -0500

    Added more test cases

--- HRSystem/src/Main.java
+++ HRSystem/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
         HRService hrService = new HRService();
 
 
-        Employee one = new Employee("One", LocalDate.of(1993, 10, 10), "engineer1", 10000, 1);
-        Employee two = new Employee("Two", LocalDate.of(1992, 10, 10), "engineer2", 20000, 0);
+        Employee one = new Employee("One", LocalDate.of(1993, 10, 10), "engineer1", 10000, 0);
+        Employee two = new Employee("Two", LocalDate.of(1992, 10, 10), "engineer2", 20000, 1);
         Employee three = new Employee("Three", LocalDate.of(1991, 10, 10), "sales1", 30000, 2);
 
         //Add 2 employee to engineering dept and 1 to sales dept. Try to add employee with assigned dept to different dept.
@@ -25,11 +25,12 @@ public class Main {
 
         printDepartmentsContent(engineeringDepartment, salesDepartment);
         printAllEmployeesStatus();
+
         //increase salary to employee One by 10%, transfer employee Two from Engineering dept to Sale dept, fire employee Three
         hrService.increaseSalary(one, 10);
         hrService.fireEmployee(three);
         try {
-            hrService.changeDepartment(two, salesDepartment);
+            hrService.changeDepartment(two, salesDepartment, "sales2");
         } catch (EntryAlreadyExistException e) {
             e.printStackTrace();
         }
@@ -37,16 +38,33 @@ public class Main {
         printDepartmentsContent(engineeringDepartment, salesDepartment);
         printAllEmployeesStatus();
 
+        //Employee One voluntarily leaves the company
         hrService.leaveCompany(one);
         printDepartmentsContent(engineeringDepartment, salesDepartment);
         printAllEmployeesStatus();
 
+        //Two annual reviews were held from year 2018 and 2019 for employee Two
+        AnnualReview annualReviewEmployeeTwo2018 = new AnnualReview(two, "Elon Musk", LocalDate.of(2018, 12, 25), 80);
+        AnnualReview annualReviewEmployeeTwo2019 = new AnnualReview(two, "Elon Musk", LocalDate.of(2019, 12, 25), 80);
+
+        //Add 2018 annual review to the database of reviews and increase salary based on result of the annual review.
+        two.addAnnualReviewToDatabase(annualReviewEmployeeTwo2018);
+        hrService.increaseSalary(two, annualReviewEmployeeTwo2018.getSalaryIncreaseInPercent());
+
+        //Add 2019 annual review to the database of reviews and increase salary based on result of the annual review.
+        two.addAnnualReviewToDatabase(annualReviewEmployeeTwo2019);
+        hrService.increaseSalary(two, annualReviewEmployeeTwo2019.getSalaryIncreaseInPercent());
+
+
+        hrService.printAnnualReviewHistory(two);
+        System.out.println();
+        System.out.println(two);
 
     }
 
     //Help method for functionality check
     public static void printDepartmentsContent(Department department1, Department department2) {
-        System.out.println("Database of employees in the company: " + Department.allEmployeesNamesInDatabase());
+        System.out.println("Database of employees in the company (active and inactive): " + Department.allEmployeesNamesInDatabase());
         System.out.println(department1.getDepartmentName() + ":");
         System.out.println(department1);
         System.out.println(department2.getDepartmentName() + ":");
