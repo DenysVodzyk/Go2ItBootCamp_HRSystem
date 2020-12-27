60783ff6d5da04ce604771408dfabaedd2c7df22
DenysVodzyk <vodzyk@gmail.com>
2020-12-27 15:20:22 -0500

    Added more test cases to check functionality of the code

--- HRSystem/src/Main.java
+++ HRSystem/src/Main.java
@@ -12,12 +12,14 @@ public class Main {
         Employee one = new Employee("One", LocalDate.of(1993, 10, 10), "engineer1", 10000, 0);
         Employee two = new Employee("Two", LocalDate.of(1992, 10, 10), "engineer2", 20000, 1);
         Employee three = new Employee("Three", LocalDate.of(1991, 10, 10), "sales1", 30000, 2);
+        Employee four = new Employee("Four", LocalDate.of(1991, 10, 10), "sales3", 5000, 0);
 
         //Add 2 employee to engineering dept and 1 to sales dept. Try to add employee with assigned dept to different dept.
         try {
             engineeringDepartment.addEmployee(one);
             engineeringDepartment.addEmployee(two);
             salesDepartment.addEmployee(three);
+            salesDepartment.addEmployee(four);
             salesDepartment.addEmployee(one);
         } catch (EntryAlreadyExistException e) {
             e.printStackTrace();
@@ -46,6 +48,7 @@ public class Main {
         //Two annual reviews were held from year 2018 and 2019 for employee Two
         AnnualReview annualReviewEmployeeTwo2018 = new AnnualReview(two, "Elon Musk", LocalDate.of(2018, 12, 25), 80);
         AnnualReview annualReviewEmployeeTwo2019 = new AnnualReview(two, "Elon Musk", LocalDate.of(2019, 12, 25), 80);
+        AnnualReview annualReviewEmployeeFour2019 = new AnnualReview(four, "Bill", LocalDate.of(2019, 12, 20), 0);
 
         //Add 2018 annual review to the database of reviews and increase salary based on result of the annual review.
         two.addAnnualReviewToDatabase(annualReviewEmployeeTwo2018);
@@ -55,10 +58,14 @@ public class Main {
         two.addAnnualReviewToDatabase(annualReviewEmployeeTwo2019);
         hrService.increaseSalary(two, annualReviewEmployeeTwo2019.getSalaryIncreaseInPercent());
 
-
+        //Add 2019 annual review for Employee Four to the database of reviews and increase salary based on result of the annual review.
+        four.addAnnualReviewToDatabase(annualReviewEmployeeFour2019);
+        hrService.increaseSalary(four, annualReviewEmployeeFour2019.getSalaryIncreaseInPercent());
+        
         hrService.printAnnualReviewHistory(two);
         System.out.println();
         System.out.println(two);
+        System.out.println(four);
 
     }
 
