004f471f6e3ed57584f575ef31c602e471d02290
DenysVodzyk <vodzyk@gmail.com>
2020-12-26 19:03:15 -0500

    Improved functionality

--- HRSystem/src/HRService.java
+++ HRSystem/src/HRService.java
@@ -6,15 +6,17 @@ public class HRService {
     public String fireEmployee(Employee employee) {
         employee.setStatus(false);
         employee.getDepartment().removeEmployee(employee);
+        Department.addEmployeeToDatabase(employee);
         return employee.getName() + ", you are fired. Here is your $" + employee.getSalary() * 0.4 +
                 ", which is 40% of your annual salary.";
     }
 
-    public void changeDepartment(Employee employee, Department newDepartment) {
-        Department currentDepartment = employee.getDepartment();
-        currentDepartment.removeEmployee(employee);
-        employee.setDepartment(newDepartment);
+
+    public void changeDepartment(Employee employee, Department newDepartment) throws EntryAlreadyExistException {
+        employee.getDepartment().removeEmployee(employee);
+        employee.setDepartment(null);
         newDepartment.addEmployee(employee);
+        employee.setDepartment(newDepartment);
     }
 
     public void increaseSalary(Employee employee, double amountToIncreasePercent) {
