4996123039abcafc0c7e7a5de907dcace73a9248
DenysVodzyk <vodzyk@gmail.com>
2020-12-27 12:45:14 -0500

    Modified some of the methods

--- HRSystem/src/HRService.java
+++ HRSystem/src/HRService.java
@@ -3,30 +3,38 @@ public class HRService {
     public HRService() {
     }
 
+    //firing employee from the company
     public String fireEmployee(Employee employee) {
         leaveCompany(employee);
         return employee.getName() + ", you are fired. Here is your $" + employee.getSalary() * 0.4 +
                 ", which is 40% of your annual salary.";
     }
 
-    public void leaveCompany(Employee employee){
+    //employee voluntary leave
+    public void leaveCompany(Employee employee) {
         employee.setStatus(false);
         employee.getDepartment().removeEmployee(employee);
         Department.addEmployeeToDatabase(employee);
     }
 
-
-    public void changeDepartment(Employee employee, Department newDepartment) throws EntryAlreadyExistException {
+    //moving from one department to another with a new role description
+    public void changeDepartment(Employee employee, Department newDepartment, String newRoleDescription) throws EntryAlreadyExistException {
         employee.getDepartment().removeEmployee(employee);
         employee.setDepartment(null);
         newDepartment.addEmployee(employee);
         employee.setDepartment(newDepartment);
+        employee.setRoleDescription(newRoleDescription);
     }
 
+    //increase a salary, either after annual review or due to other circumstance
     public void increaseSalary(Employee employee, double amountToIncreasePercent) {
         double newSalary = employee.getSalary() * (1 + amountToIncreasePercent / 100);
         employee.setSalary(newSalary);
     }
 
+    //print out employee's annual review history
+    public void printAnnualReviewHistory(Employee employee) {
+        System.out.println(employee.getAnnualReviewDatabase());
+    }
 
 }
