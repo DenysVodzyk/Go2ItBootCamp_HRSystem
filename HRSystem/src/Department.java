01565e300325f408bfcf519419281eb6219201b4
DenysVodzyk <vodzyk@gmail.com>
2020-12-26 19:02:21 -0500

    Added more functionality

--- HRSystem/src/Department.java
+++ HRSystem/src/Department.java
@@ -1,11 +1,9 @@
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Department {
     private Set<Employee> employees = new HashSet<>();
     private String departmentName;
-
+    private static Map<String, Boolean> allEmployeesDatabase = new HashMap<>();
 
     public Department() {
     }
@@ -14,27 +12,65 @@ public class Department {
         this.departmentName = departmentName;
     }
 
-    public void addEmployee(Employee employee) {
-        employees.add(employee);
+    public String getDepartmentName() {
+        return departmentName;
     }
 
+    //add employee to department, throw exception in case if department is already assigned to employee.
+    public void addEmployee(Employee employee) throws EntryAlreadyExistException {
+        if (employee.getDepartment() != null) {
+            throw new EntryAlreadyExistException("Employee " + employee.getName() + " is already a part of " + employee.getDepartment().getDepartmentName() + " department.");
+        }
+        employee.setDepartment(Department.this);
+        employees.add(employee);
+        addEmployeeToDatabase(employee);
+    }
+
+    public static void addEmployeeToDatabase(Employee employee) {
+        allEmployeesDatabase.put(employee.getName(), employee.isStatus());
+    }
+
+    //quantity of employees in the company
+    public static int numberOfEmployeesInDatabase() {
+        return allEmployeesDatabase.size();
+    }
+
+    //returning all employees in the company, just names
+    public static Set<String> allEmployeesNamesInDatabase() {
+        return allEmployeesDatabase.keySet();
+    }
+
+    //Printing all active employees in the company
+    public static void printActiveEmployees() {
+        for (Map.Entry<String, Boolean> activeEmployee : allEmployeesDatabase.entrySet()) {
+            if (activeEmployee.getValue()) {
+                System.out.println(activeEmployee);
+            }
+        }
+    }
+
+    //Printing all inactive employees in the company
+    public static void printInactiveEmployees() {
+        for (Map.Entry<String, Boolean> inactiveEmployee : allEmployeesDatabase.entrySet()) {
+            if (!inactiveEmployee.getValue()) {
+                System.out.println(inactiveEmployee);
+            }
+        }
+    }
+
+    //remove employee from department list. Employee still stays within the company database.
     public void removeEmployee(Employee employee) {
         employees.remove(employee);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Department that = (Department) o;
-        return employees.equals(that.employees) &&
-                departmentName.equals(that.departmentName);
+    //print names of employees that are in the department
+    public void printDepartmentEmployees() {
+        for (Employee employee : employees) {
+            System.out.print(employee.getName() + " ");
+        }
+        System.out.println("");
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(employees, departmentName);
-    }
 
     @Override
     public String toString() {
